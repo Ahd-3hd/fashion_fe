93f0278d6349dbfae3a9b11eda02155a73b02274
Ahd <dafuq.ahd@gmail.com>
2020-03-11 18:54:38 +0300

    add fav icon to item, add new tag to item, add item details

--- src/components/CollectionItem/index.js
+++ src/components/CollectionItem/index.js
@@ -1,27 +1,59 @@
 import React from "react";
 import styled from "styled-components";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 const ItemWrapper = styled.div`
-  position: relative;
+  // set to absolute to handle swiping effect, but might break positioning.
+  position: absolute;
   border-radius: 0.5rem;
   > .item-media {
+    position: relative;
     width: 160px;
     height: 220px;
     background: url(${props => props.image}) no-repeat;
     background-size: cover;
     border-radius: 0.5rem;
+    .fav-button {
+      background: rgba(255, 255, 255, 0.6);
+      border: none;
+      padding: 0.4rem 0.6rem;
+      margin: 0.5rem;
+      position: absolute;
+      right: 0;
+      border-radius: 0.3rem;
+      color: #8e4368;
+    }
+    p {
+      position: absolute;
+      bottom: 0;
+      margin-left: 0.4rem;
+      font-size: 0.85rem;
+      font-weight: bold;
+      padding: 0.4rem 0.6rem;
+      background: rgba(255, 255, 255, 0.6);
+      border-radius: 0.3rem;
+    }
   }
   > .item-details {
     p {
       margin: 0;
       font-size: 0.85rem;
+      color: #523448;
+    }
+    .item-title {
+      font-weight: bold;
     }
   }
 `;
 const CollectionItem = props => {
   return (
     <ItemWrapper image={props.designer}>
-      <div className="item-media"></div>
+      <div className="item-media">
+        <button className="fav-button">
+          <FontAwesomeIcon icon={faHeart} />
+        </button>
+        <p>New</p>
+      </div>
       <div className="item-details">
         <p className="item-title">Item Title</p>
         <p className="item-price">$99,99</p>

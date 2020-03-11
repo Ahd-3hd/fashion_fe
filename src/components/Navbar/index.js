958cc0872bf6191fc3bae43963d22eb79bced76e
Ahd <dafuq.ahd@gmail.com>
2020-03-11 15:21:01 +0300

    add burger menu to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 const NavbarWrapper = styled.nav`
   position: fixed;
   right: 0;
   top: 0;
+  .burger-menu {
+    margin: 1rem;
+    font-size: 1.2rem;
+    color: #523448;
+  }
 `;
 
 const Navbar = () => {
-  return <NavbarWrapper>hi navbar</NavbarWrapper>;
+  return (
+    <NavbarWrapper>
+      <FontAwesomeIcon icon={faBars} className="burger-menu" />
+    </NavbarWrapper>
+  );
 };
 
 export default Navbar;

9b06d7ef0650f821b7cf3d202e1140d095284676
Ahd <dafuq.ahd@gmail.com>
2020-03-11 15:22:10 +0300

    add navbar to header component; use useEffect hook to add an event listener to the window to animate headers height

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Navbar from "../Navbar";
 import headerBackground from "../../img/abaya7.jpg";
@@ -6,11 +6,21 @@ const HeaderWrapper = styled.header`
   position: relative;
   background: url(${headerBackground}) no-repeat;
   background-size: cover;
-  height: 50vh;
+  height: ${props => props.headerHeight};
+  transition: 0.7s ease-in-out;
 `;
 const Header = () => {
+  const [height, setHeight] = useState(false);
+
+  //Handle scroll height value
+  useEffect(() => {
+    window.addEventListener("scroll", e => {
+      window.pageYOffset >= 30 ? setHeight(true) : setHeight(false);
+    });
+  }, []);
+
   return (
-    <HeaderWrapper>
+    <HeaderWrapper headerHeight={height ? "30vh" : "50vh"}>
       <Navbar />
     </HeaderWrapper>
   );

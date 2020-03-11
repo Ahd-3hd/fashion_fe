b49305ca6c7711d254a5e0753567d09fc42029b8
Ahd <dafuq.ahd@gmail.com>
2020-03-11 17:43:49 +0300

    create Collection component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-
+import Collection from "./components/Collection";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Collection />
       <div style={{ height: "200vh" }}></div>
     </div>
   );
---
+++ src/components/Collection/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const CollectionWrapper = styled.section``;
+const Collection = () => {
+  return <CollectionWrapper></CollectionWrapper>;
+};
+
+export default Collection;

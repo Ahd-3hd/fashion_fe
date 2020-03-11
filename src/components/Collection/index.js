2a794d958a0e684fdb7669c95dc14f1a6a456787
Ahd <dafuq.ahd@gmail.com>
2020-03-11 17:52:27 +0300

    create collections top bar containing the title and the link to show all; style topbar

--- src/components/Collection/index.js
+++ src/components/Collection/index.js
@@ -1,9 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 
-const CollectionWrapper = styled.section``;
+const CollectionWrapper = styled.section`
+  margin-top: 2.5rem;
+  .top-bar {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding: 0 1rem;
+    > p {
+      margin: 0;
+      font-weight: bold;
+      font-size: 0.85rem;
+      letter-spacing: 0.15em;
+      color: #523448;
+    }
+    > a {
+      font-size: 11px;
+      color: #d8a890;
+      text-decoration: none;
+    }
+  }
+`;
 const Collection = () => {
-  return <CollectionWrapper></CollectionWrapper>;
+  return (
+    <CollectionWrapper>
+      <div className="top-bar">
+        <p>DESIGNER'S COLLECTION</p>
+        <a href="#deadsies">SHOW ALL</a>
+      </div>
+    </CollectionWrapper>
+  );
 };
 
 export default Collection;

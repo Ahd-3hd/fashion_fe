1c52aa95fc072d373ec22e6ebb6e1444bac9a69b
Ahd <dafuq.ahd@gmail.com>
2020-03-12 18:13:46 +0300

    import couple of images and turn the passed props into an array of images

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Collection from "./components/Collection";
-import itemImage from "./img/abaya12.jpg";
+import itemImage0 from "./img/abaya12.jpg";
+import itemImage1 from "./img/abaya3.jpg";
+import itemImage2 from "./img/abaya5.jpg";
+import itemImage3 from "./img/abaya9.jpg";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Collection designer={itemImage} />
+      <Collection designer={[itemImage0, itemImage1, itemImage2, itemImage3]} />
       <div style={{ height: "200vh" }}></div>
     </div>
   );

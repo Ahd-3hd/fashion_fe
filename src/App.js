35e8c2d3f162d5422f48b6cb4811c77538a92f63
Ahd <dafuq.ahd@gmail.com>
2020-03-13 01:10:51 +0300

    add routing to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Collection from "./components/Collection";
@@ -7,19 +8,24 @@ import itemImage0 from "./img/abaya12.jpg";
 import itemImage1 from "./img/abaya3.jpg";
 import itemImage2 from "./img/abaya5.jpg";
 import itemImage3 from "./img/abaya9.jpg";
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Collection
-        title={"DESIGNER'S COLLECTION"}
-        designer={[itemImage0, itemImage1, itemImage2, itemImage3]}
-      />
-      <Collection
-        title={"TOP TRENDING"}
-        designer={[itemImage1, itemImage0, itemImage3, itemImage2]}
-      />
-      <Explore />
+      <Switch>
+        <Route exact path="/">
+          <Header />
+          <Collection
+            title={"DESIGNER'S COLLECTION"}
+            designer={[itemImage0, itemImage1, itemImage2, itemImage3]}
+          />
+          <Collection
+            title={"DESIGNER'S COLLECTION"}
+            designer={[itemImage0, itemImage1, itemImage2, itemImage3]}
+          />
+          <Explore />
+        </Route>
+      </Switch>
     </div>
   );
 }
